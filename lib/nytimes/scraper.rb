72176cc8557b97128471c2281191ca027f01c521
LIManuel <leonmalisov@gmail.com>
2020-03-19 17:19:50 -0400

    first commit

---
+++ lib/nytimes/scraper.rb
@@ -0,0 +1,8 @@
+require "nytimes/scraper/version"
+
+module Nytimes
+  module Scraper
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/nytimes/scraper/version.rb
@@ -0,0 +1,5 @@
+module Nytimes
+  module Scraper
+    VERSION = "0.1.0"
+  end
+end
